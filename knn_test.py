74debb193f54bd04421a42f3886360121685acf4
Marcelo Arteaga <jarteaga@agetic.gob.bo>
2017-08-01 23:05:01 -0400

    nuevo

---
+++ knn_test.py
@@ -0,0 +1,14 @@
+import numpy as np
+from sklearn import neighbors, datasets
+
+n_neighbors = 15
+
+iris = datasets.load_iris()
+x = iris.data[:, :2]
+y = iris.target
+
+clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
+clf.fit(x, y)
+
+
+print(y)
